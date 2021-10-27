9daffbe8597018ddb49b8b72f16423de427148e3
jacob-armiger <armigersemail@gmail.com>
2021-10-27 19:32:51 -0400

    Add styling to component

--- components/SearchFilter.tsx
+++ components/SearchFilter.tsx
@@ -2,6 +2,7 @@
 import react, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { searchRegions, searchTags } from '../utils/interfaces';
+import styles from './SearchFilter.module.css';
 
 const SearchFilter: react.FC = () => {
     // useState for tag selection
@@ -19,53 +20,55 @@ const SearchFilter: react.FC = () => {
     // useState for Key word search
     const[ searchWord, setSearchWord ] = useState('');
 
-    // Dynamically Route to new page on user search
-    const router = useRouter();
-    const getSearch = () => {
-        router.push(`search/${region}/${tag}/${searchWord}`);
-    };
-
-
+    // Object to return to database
     let searchParameters = {
+        // Values are updated on form change
         tagParam: tag,
         regionParam: region,
         searchWordParam: searchWord,
         dateParam: ''
     };
 
+    // Dynamically Route to new page based on user criteria
+    const router = useRouter();
+    const getSearch = () => {
+        // Actions to take when form is submitted
+        router.push(`search/${region}/${tag}/${searchWord}`);
+
+        // NOTE: needs proper return;
+        return searchParameters;
+    };
+
     return (
         <div>
-            <form>
-                <label>
-                    Search
+            <form >
+                <div className={styles.search}>
                     <input
                         type="text"
+                        className={styles.searchTerm}
                         onChange={(word) => {
                             // useState's `setsearchWord` assigns to value to `searchWord`
                             // NOTE: takes you to 404 page since there is no search data
                             return setSearchWord(word.target.value);
                         }}
                     />
-                </label>
-                <button onClick={getSearch}>Submit</button>
+                    <button type="submit" className={styles.searchButton} onClick={getSearch}></button>
+                </div>
+
+                <select className={styles.select} onChange={handleRegionChange}>
+                    <option value="all">Select Region</option>
+                    {searchRegions.map((regionSelect) => {
+                        return <option key={regionSelect.label}>{regionSelect.value}</option>;
+                    })}
+                </select>
+
+                <select className={styles.select} onChange={handleTagChange}>
+                    <option value="all">Select Tag</option>
+                    {searchTags.map((tagSelect) => {
+                        return <option key={tagSelect.label}>{tagSelect.value}</option>;
+                    })}
+                </select>
             </form>
-
-            <select onChange={handleRegionChange}>
-                <option value="all">Select Region</option>
-                {searchRegions.map((regionSelect) => {
-                    return <option key={regionSelect.label}>{regionSelect.value}</option>;
-                })}
-            </select>
-
-            <select onChange={handleTagChange}>
-                <option value="all">Select Tag</option>
-                {searchTags.map((tagSelect) => {
-                    return <option key={tagSelect.label}>{tagSelect.value}</option>;
-                })}
-            </select>
-            <p>{searchParameters.regionParam}</p>
-            <p>{searchParameters.tagParam}</p>
-            <p>{searchParameters.searchWordParam}</p>
         </div>
     );
 };
